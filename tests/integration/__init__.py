b81dc21473f62b6e3328a9276355eed793ceeec7
Jaume Marhuenda <jaumemarhuenda@gmail.com>
2018-03-17 19:11:46 +0100

    PYTHON-343 SSL support for Twisted

--- tests/integration/__init__.py
+++ tests/integration/__init__.py
@@ -95,7 +95,7 @@ USE_CASS_EXTERNAL = bool(os.getenv('USE_CASS_EXTERNAL', False))
 KEEP_TEST_CLUSTER = bool(os.getenv('KEEP_TEST_CLUSTER', False))
 SIMULACRON_JAR = os.getenv('SIMULACRON_JAR', None)
 
-default_cassandra_version = Version('3.11')
+default_cassandra_version = '3.11'
 
 CASSANDRA_IP = os.getenv('CASSANDRA_IP', '127.0.0.1')
 CASSANDRA_DIR = os.getenv('CASSANDRA_DIR', None)
